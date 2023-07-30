8071ab33860e4dea5aad6365da9683c2ea01d117
Kota Suzuki <addressuzuki@gmail.com>
2023-07-30 10:28:24 +0900

    コミット漏れ

---
+++ lib/infra/providers/quote_repository_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'quote_repository_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$quoteRepositoryHash() => r'4bbea6eef5d56c1bd65656a2787492a3b60b7a55';
+
+/// See also [quoteRepository].
+@ProviderFor(quoteRepository)
+final quoteRepositoryProvider = FutureProvider<QuoteRepository>.internal(
+  quoteRepository,
+  name: r'quoteRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$quoteRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef QuoteRepositoryRef = FutureProviderRef<QuoteRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
