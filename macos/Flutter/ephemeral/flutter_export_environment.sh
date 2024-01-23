d96a02ed7659cf81bece9bd03b1fd7a35f1aee5e
Kota Suzuki <addressuzuki@gmail.com>
2024-01-23 10:03:13 +0900

    アイコンの変更

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/suzukikouta/fvm/versions/3.13.6"
+export "FLUTTER_ROOT=/Users/suzukikouta/fvm/versions/3.10.6"
 export "FLUTTER_APPLICATION_PATH=/Users/suzukikouta/StudioProjects/meigen_finder"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
